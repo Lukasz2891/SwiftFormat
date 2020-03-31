3fa917518fe4ddb5e2bf28a8b91df81ac9f8a28a
Lukasz Kalbarczyk <lukasz.kalbarczyk@stepstone.com>
2020-03-31 19:39:37 +0200

    Stst rules tuned; ststRedundantTypeName rule added

--- Tests/RulesTests.swift
+++ Tests/RulesTests.swift
@@ -11056,12 +11056,33 @@ class RulesTests: XCTestCase {
             return 5
 
         }
+
+        var somethingElse: Int {
+            return 6
+
+        }
         """
         let output = """
         var something: Int { 5 }
+
+        var somethingElse: Int { 6 }
         """
 
         XCTAssertEqual(try format(input, rules: [FormatRules.ststRedundantOneLineVarReturn], options: .default), output)
+    }
+
+    func testStStRedundantOneLineVarReturnInsideFunctionNoChange() {
+        let input = """
+        var offset: CGFloat = 0
+
+        if subviews.isEmpty {
+            return offset
+        }
+        """
+        let output = input
+
+        let options = FormatOptions(removeBlankLines: true)
+        XCTAssertEqual(try format(input, rules: [FormatRules.ststRedundantOneLineVarReturn, FormatRules.consecutiveSpaces], options: options), output)
     }
 
     func testStstLinebreakAfterGuard() {
@@ -11142,5 +11163,91 @@ class RulesTests: XCTestCase {
 
         let options = FormatOptions(removeBlankLines: true)
         XCTAssertEqual(try format(input, rules: [FormatRules.ststLinebreakBeforeReturn], options: options), output)
+    }
+
+    func testStstRedundantTypeNameBool() {
+        let input = "var something: Bool = true"
+        let output = "var something = true"
+
+        let options = FormatOptions(removeBlankLines: true)
+        XCTAssertEqual(try format(input, rules: [FormatRules.ststRedundantTypeName], options: options), output)
+    }
+
+    func testStstRedundantTypeNameString() {
+        let input = "var something: String = \"value\" "
+        let output = "var something = \"value\" "
+
+        let options = FormatOptions(removeBlankLines: true)
+        XCTAssertEqual(try format(input, rules: [FormatRules.ststRedundantTypeName], options: options), output)
+    }
+
+    func testStstRedundantTypeNameStatic() {
+        let input = "var something: Type = Type.make()!"
+        let output = "var something = Type.make()!"
+
+        let options = FormatOptions(removeBlankLines: true)
+        XCTAssertEqual(try format(input, rules: [FormatRules.ststRedundantTypeName], options: options), output)
+    }
+
+    func testStstRedundantTypeName() {
+        let input = "var something: Type = Type(value: 5)"
+        let output = "var something = Type(value: 5)"
+
+        let options = FormatOptions(removeBlankLines: true)
+        XCTAssertEqual(try format(input, rules: [FormatRules.ststRedundantTypeName], options: options), output)
+    }
+    
+    func testStstRedundantTypeNameCGFloat() {
+        let input = "var something: CGFloat = CGFloat(apply(233))"
+        let output = "var something: CGFloat = apply(233)"
+
+        let options = FormatOptions(removeBlankLines: true)
+        XCTAssertEqual(try format(input, rules: [FormatRules.ststRedundantTypeName], options: options), output)
+    }
+
+    func testStstRedundantTypeNameEnum() {
+        let input = "var something: UIColor = UIColor.red"
+        let output = "var something = UIColor.red"
+
+        let options = FormatOptions(removeBlankLines: true)
+        XCTAssertEqual(try format(input, rules: [FormatRules.ststRedundantTypeName], options: options), output)
+    }
+
+    func testStstRedundantTypeNameInsideFunArgsNoChange() {
+         let input = "func doSomething(with test: Bool = true)"
+
+         let options = FormatOptions(removeBlankLines: true)
+         XCTAssertEqual(try format(input, rules: [FormatRules.ststRedundantTypeName], options: options), input)
+     }
+
+    func testStstRedundantTypeNameFailableInitNoChange() {
+         let input = "let image: UIImage = UIImage(named: \"aaa\")!"
+
+         let options = FormatOptions(removeBlankLines: true)
+         XCTAssertEqual(try format(input, rules: [FormatRules.ststRedundantTypeName], options: options), input)
+     }
+
+    func testStstLinebreakAfterClass() {
+        let input = """
+        class StepBaseView<ContentView: UIView>: SCBaseView, SingleStepView {
+            private struct Constants {
+                let backgroundColor = UIColor.white
+                let horizontalMargin: CGFloat = 16
+            }
+        }
+        """
+        let output = """
+        class StepBaseView<ContentView: UIView>: SCBaseView, SingleStepView {
+
+            private struct Constants {
+
+                let backgroundColor = UIColor.white
+                let horizontalMargin: CGFloat = 16
+            }
+        }
+        """
+
+        let options = FormatOptions(removeBlankLines: true)
+        XCTAssertEqual(try format(input, rules: [FormatRules.ststLinebreakAfterClassExtensionStruct], options: options), output)
     }
 }
